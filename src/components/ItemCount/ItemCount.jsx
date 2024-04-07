bf57a791507b95f6cf59e39ab978cc2b878ad593
enderaraya <ender.araya.dev@gmail.com>
2024-04-06 23:14:42 -0300

    Entrega Coder

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,28 +1,48 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-const ItemCount = ({ initial = 1, stock, onAdd } ) => {
-    const [count, setCount] = useState(initial)
+const Alert = ({ message }) => {
+    return (
+        <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', background: 'linear-gradient(to right, #5B5EF2, #B6B7F9)', padding: '20px', border: '1px solid black', borderRadius: '5px', zIndex: '1000' }}>
+            <p>{message}</p>
+        </div>
+    );
+};
+
+const ItemCount = ({ initial = 1, stock, productName, onAdd,price }) => {
+    const [count, setCount] = useState(initial);
+    const [showAlert, setShowAlert] = useState(false);
 
     const decrement = () => {
-        if(count > 1) {
-            setCount(prev => -1)
+        if (count > 1) {
+            setCount(prev => prev - 1);
         }
-    }
+    };
 
     const increment = () => {
-        if(count < stock) {
-            setCount(prev => +1)
+        if (count < stock) {
+            setCount(prev => prev + 1);
         }
-    }
+    };
+
+    const handleAddToCart = () => {
+        const total = price * count;
+        const message = `Haz agregado ${count} ${productName} con un valor total de $${total}`;
+        setShowAlert(true);
+        setTimeout(() => {
+            setShowAlert(false);
+        }, 2000); // Oculta la alerta después de 2 segundos
+        onAdd(count);
+    };
 
     return (
         <article>
-            <h3 style={{display: 'flex', justifyContent: 'center', alignItems: 'space-between'}}>{count}</h3>
+            <h3 style={{ display: 'flex', justifyContent: 'center', alignItems: 'space-between' }}>{count}</h3>
             <button onClick={decrement}>-</button>
-            <button onClick={() => onAdd(count)}>Agregar al carrito</button>
+            <button onClick={handleAddToCart}>Agregar al carrito</button>
             <button onClick={increment}>+</button>
+            {showAlert && <Alert message={`Haz agregado ${count} ${productName} con un valor total de $${price * count}`} />}
         </article>
-    )
-}
+    );
+};
 
-export default ItemCount
+export default ItemCount;
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -12,7 +12,7 @@ const ItemDetail = ({id, name, category, price, img, description, stock }) => {
             <h4>categoria: {category}</h4>
             <h4>Descripcion: {description}</h4>
             <div style={{display: 'flex', justifyContent: 'center', alignItems: 'space-between'}}>
-                <ItemCount stock={stock}/>
+                <ItemCount stock={stock } productName={name} price={price}/>
             </div>
         </article>
     )
