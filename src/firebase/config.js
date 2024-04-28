2d7e9eae746564354178b9e6c59ac9ef26cc3a4d
enderaraya <ender.araya.dev@gmail.com>
2024-04-27 23:35:01 -0400

    correcion devolución
    correcion y implementacion de firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBWfVS-bJIrEJLDLZ0g2ScEYV9G2anDFF0",
+    authDomain: "eacoderreactjs.firebaseapp.com",
+    projectId: "eacoderreactjs",
+    storageBucket: "eacoderreactjs.appspot.com",
+    messagingSenderId: "30260681810",
+    appId: "1:30260681810:web:433ecb46d0108e191d938b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
